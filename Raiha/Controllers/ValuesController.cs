17699d5ef17961d3e303c78ccb430688cec0104e
tangredon <9369471+tangredon@users.noreply.github.com>
2022-01-07 02:15:55 +0200

    Reorganize terraform files

--- Raiha/Controllers/ValuesController.cs
+++ Raiha/Controllers/ValuesController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Raiha.Web.Controllers;
+namespace Raiha.Controllers;
 
 [Route("api/[controller]")]
 public class ValuesController : ControllerBase
--- Raiha/LocalEntryPoint.cs
+++ Raiha/LocalEntryPoint.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Raiha.Web;
+namespace Raiha;
 
 /// <summary>
 /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel web server.
