350477800ef91da3a02734ca34c835ac2442ddac
tangredon <9369471+tangredon@users.noreply.github.com>
2022-01-07 12:54:11 +0200

    Add aws deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,21 +15,19 @@ terraform {
   }
 }
 
-# data "aws_api_gateway_rest_api" "api_raiha" {
-#   name = "raiha-stack"
-# }
-
 data "aws_caller_identity" "current" {}
 
 locals {
-    prefix              = "raiha"
-    account_id          = data.aws_caller_identity.current.account_id
-    ecr_repository_name = "raiha"
-    ecr_image_tag       = "0.1.0-unstable.22"
+  prefix              = "raiha"
+  account_id          = data.aws_caller_identity.current.account_id
+  ecr_repository_name = "raiha"
 }
 
 variable "aws_access" {}
 variable "aws_secret" {}
+
+variable "stage_name" {default = "test"}
+variable "image_tag" {}
 
 provider "aws" {
   region     = "eu-central-1"
@@ -38,8 +36,8 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "lambda" {
-    name = "${local.prefix}-lambda-role"
-    assume_role_policy = <<EOF
+  name = "${local.prefix}-lambda-role"
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -52,135 +50,120 @@ resource "aws_iam_role" "lambda" {
         }
     ]
 }
-    EOF
+  EOF
 }
 
-resource "aws_iam_role_policy" "api-invoker" {
-    role     = aws_iam_role.lambda.id
-    policy   = data.aws_iam_policy_document.execute-api.json
-}
-
-data "aws_iam_policy_document" "execute-api" {
-    statement {
-     sid = "all"
-     actions = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-     ]
-     resources = [
-       "*"
-     ]
-   }
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 data "aws_ecr_repository" "repo" {
-  name = "raiha"
+  name = local.ecr_repository_name
 }
 
-data "aws_ecr_image" "lambda_image" {
-    repository_name = local.ecr_repository_name
-    image_tag       = local.ecr_image_tag
+data "aws_ecr_image" "image" {
+  repository_name = local.ecr_repository_name
+  image_tag       = var.image_tag
 }
 
-resource "aws_cloudwatch_log_group" "test-app-loggroup" {
-  name              = "/aws/lambda/raiha-lambda"
-  retention_in_days = 90
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${local.prefix}-lambda"
+  retention_in_days = 7
 }
 
-resource "aws_lambda_function" git {
-    depends_on = [
-        aws_cloudwatch_log_group.test-app-loggroup
-    ]
+resource "aws_lambda_function" "main" {
+  depends_on = [
+      aws_cloudwatch_log_group.lambda_logs
+  ]
 
-    function_name = "${local.prefix}-lambda"
-    role = aws_iam_role.lambda.arn
-    // handler = "Raiha::Raiha.LambdaEntryPoint::FunctionHandlerAsync"
-    timeout = 300
-    image_uri = "${data.aws_ecr_repository.repo.repository_url}@${data.aws_ecr_image.lambda_image.id}"
-    package_type = "Image"
+  function_name = "${local.prefix}-lambda"
+  role = aws_iam_role.lambda.arn
+  timeout = 5
+  image_uri = "${data.aws_ecr_repository.repo.repository_url}@${data.aws_ecr_image.image.id}"
+  package_type = "Image"
 }
 
-resource "aws_api_gateway_rest_api" "example" {
-  name        = "ServerlessExample"
+resource "aws_api_gateway_rest_api" "lambda" {
+  name        = "raiha-api"
   description = "Terraform Serverless Application Example"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+  parent_id   = aws_api_gateway_rest_api.lambda.root_resource_id
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  rest_api_id   = aws_api_gateway_rest_api.lambda.id
+  resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.git.invoke_arn}"
+  uri                     = aws_lambda_function.main.invoke_arn
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  resource_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
+  rest_api_id   = aws_api_gateway_rest_api.lambda.id
+  resource_id   = aws_api_gateway_rest_api.lambda.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.git.invoke_arn}"
+  uri                     = aws_lambda_function.main.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "example" {
+resource "aws_api_gateway_deployment" "lambda" {
   depends_on = [
     aws_api_gateway_integration.lambda,
     aws_api_gateway_integration.lambda_root,
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  stage_name  = "test"
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+  stage_name  = var.stage_name
 }
 
-resource "aws_lambda_permission" "apigw" {
+resource "aws_lambda_permission" "apigateway" {
     statement_id  = "AllowAPIGatewayInvoke"
     action        = "lambda:InvokeFunction"
-    function_name = "${aws_lambda_function.git.arn}"
+    function_name = aws_lambda_function.main.arn
     principal     = "apigateway.amazonaws.com"
 
-    # The /*/* portion grants access from any method on any resource
-    # within the API Gateway "REST API".
-    source_arn = "${replace(aws_api_gateway_deployment.example.execution_arn, "test", "")}*/*"
+    # The /*/* portion grants access from any method on any resource within the API Gateway "REST API".
+    source_arn = "${replace(aws_api_gateway_deployment.lambda.execution_arn, var.stage_name, "")}*/*"
 }
 
-resource "aws_lambda_permission" "test-app-allow-cloudwatch" {
-  statement_id  = "test-app-allow-cloudwatch"
+resource "aws_lambda_permission" "cloudwatch" {
+  statement_id  = "AllowCloudwatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.git.arn}"
+  function_name = aws_lambda_function.main.arn
   principal     = "logs.eu-central-1.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_log_group.test-app-loggroup.arn}:*"
+  source_arn    = "${aws_cloudwatch_log_group.lambda_logs.arn}:*"
 }
 
-# resource "aws_cloudwatch_log_subscription_filter" "test-app-cloudwatch-sumologic-lambda-subscription" {
-#   depends_on      = [
-#     aws_lambda_permission.test-app-allow-cloudwatch
-#   ]
-#   name            = "cloudwatch-sumologic-lambda-subscription"
-#   log_group_name  = "${aws_cloudwatch_log_group.test-app-loggroup.name}"
-#   filter_pattern  = ""
-#   destination_arn = "${aws_lambda_function.git.arn}"
-# }
+data "aws_api_gateway_domain_name" "raiha" {
+  domain_name = "serverless.tangredon.com"
+}
+
+resource "aws_api_gateway_base_path_mapping" "raiha" {
+  api_id      = aws_api_gateway_rest_api.lambda.id
+  stage_name  = var.stage_name
+  domain_name = data.aws_api_gateway_domain_name.raiha.domain_name
+  base_path   = "${local.prefix}_"
+}
